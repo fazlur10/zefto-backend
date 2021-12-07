2740f52f5b05174726855c4f55b3c0e44bdc8a2a
Faslur Rahman <faslur.rahman@besolution.com>
2021-12-07 17:03:48 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00e1717b2394789573ba33359dad483a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2007fe21-9613-42b2-be15-6a6c9ca083dd'
+};
